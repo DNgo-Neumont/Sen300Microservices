49c96b15d6112bbf201befcdc85b3bbdbd58bd90
docsteampunk <79662507+docsteampunk@users.noreply.github.com>
2022-07-07 15:31:29 -0600

    Delete ItemAPI directory

--- ItemAPI/Item.cs
+++
@@ -1,7 +0,0 @@
-public class Item
-{
-    public long Id { get; set; }
-    public string? Title { get; set; }
-    public string? Description { get; set;}
-    public decimal unitPrice { get; set; } = 0.0m;
-}
