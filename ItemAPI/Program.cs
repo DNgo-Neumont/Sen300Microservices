3e2412511ab2da5f787b6c5bd0d86c63b2ba45b2
docsteampunk <larmore.daniel@gmail.com>
2022-07-08 14:13:28 -0600

    Added docker stuff

--- ItemAPI/Program.cs
+++ ItemAPI/Program.cs
@@ -1,6 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello Worlds!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+//builder.Services.AddDbContext<QuestDB>(opt => opt.UseInMemoryDatabase("QuestBoard"));
+builder.Services.AddDbContext<ItemDB>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("docker_db1")));
+
+var app = builder.Build();
+app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
