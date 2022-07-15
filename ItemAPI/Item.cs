4aaa12d3cd46e003cf2b07ee585e3e53124ec2a9
docsteampunk <larmore.daniel@gmail.com>
2022-07-15 14:49:06 -0600

    Made ItemAPI

--- ItemAPI/Item.cs
+++ ItemAPI/Item.cs
@@ -3,5 +3,5 @@ public class Item
     public long Id { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set;}
-    public decimal unit_price { get; set; } = 0.0m;
+    public double unit_price { get; set; } = 0.0;
 }
