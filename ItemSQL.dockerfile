df5865b8492c77f4ac95b9a99caf16a8244ca6d4
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2022-07-22 19:50:05 -0600

    Added proper SQL auto building tables.

--- ItemSQL.dockerfile
+++
@@ -1,8 +0,0 @@
-FROM mcr.microsoft.com/mssql/server:2019-latest
-
-ENV ACCEPT_EULA=Y
-ENV SA_PASSWORD=abc123!!23
-
-ENTRYPOINT [ "/bin/bash", "entrypoint.sh" ]
-
-CMD [ "/opt/mssql/bin/sqlservr" ]
---
+++ itemSqlSetup/entrypointItem.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# set -e
+
+
+# if [ "$1" = '/opt/mssql/bin/sqlservr' ]; then
+#   if [ ! -f /tmp/app-initialized ]; then
+#     function initialize_app_database() {
+#       #/opt/msql-tools/bin/sqlcmd -S localhost -U sa -P abc123!!23 -d master -i init_sql_item.sql
+      
+#       touch /tmp/app-initialized
+#     }
+    
+#     initialize_app_database & itemSQLinit.sh
+#   fi
+# fi
+
+# exec "$@"
+/opt/mssql/bin/sqlservr & /home/itemSQLinit.sh
